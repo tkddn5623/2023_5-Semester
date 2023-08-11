10702de1239e2157e1ca6e7e8afa7114d55e1443
tkddn5623 <tkddn5623@g.skku.edu>
2023-08-11 18:07:09 +0900

    Trivial change.

--- _myLibrary/_myLibrary/Data Structures/ArrayHeap.c
+++ _myLibrary/_myLibrary/Data Structures/ArrayHeap.c
@@ -3,74 +3,73 @@
 /*
 typedef int Element;
 typedef struct {
-	Element item;
-	int priority;
+    Element item;
+    int priority;
 } HNode;
 typedef struct {
-	HNode* nodes;
-	int size;
+    HNode* nodes;
+    int size;
 } ArrayHeap;
 
 */
 ArrayHeap* AH_new(int max) {
-	ArrayHeap* pheap;
-	if ((pheap = calloc(1, sizeof(ArrayHeap))) == NULL) exit(1);
-	if ((pheap->nodes = calloc(max + 1, sizeof(HNode))) == NULL) exit(1);
-	return pheap;
+    ArrayHeap* pheap;
+    if ((pheap = calloc(1, sizeof(ArrayHeap))) == NULL) exit(1);
+    if ((pheap->nodes = calloc(max + 1, sizeof(HNode))) == NULL) exit(1);
+    return pheap;
 }
 void AH_deleteHeap(ArrayHeap* pheap) {
-	free(pheap->nodes);
-	free(pheap);
+    free(pheap->nodes);
+    free(pheap);
 }
 int AH_isEmpty(ArrayHeap* pheap) {
-	return pheap->size == 0;
+    return pheap->size == 0;
 }
 void AH_push(ArrayHeap* pheap, Element item, int priority) {
-	HNode newNode;
-	int index = pheap->size + 1;
-	while (index > 1) {
-		int parentIndex = index / 2;
-		if (priority < pheap->nodes[parentIndex].priority) { 
-			//Minheap
-			pheap->nodes[index] = pheap->nodes[parentIndex];
-			index = parentIndex;
-		}
-		else break;
-	}
-	newNode.item = item;
-	newNode.priority = priority;
-	pheap->nodes[index] = newNode;
-	pheap->size++;
+    HNode newNode;
+    int index = pheap->size + 1;
+    while (index > 1) {
+        int parentIndex = index / 2;
+        if (priority < pheap->nodes[parentIndex].priority) {
+            //Minheap
+            pheap->nodes[index] = pheap->nodes[parentIndex];
+            index = parentIndex;
+        }
+        else break;
+    }
+    newNode.item = item;
+    newNode.priority = priority;
+    pheap->nodes[index] = newNode;
+    pheap->size++;
 }
 Element AH_pop(ArrayHeap* pheap) {
-	const int size = pheap->size;
-	const Element topitem = pheap->nodes[1].item;
-	const HNode last = pheap->nodes[size];
-	int left, pickedChild, parentIndex = 1;
-	while ((left = parentIndex * 2) <= size) {
-		if (left == size) pickedChild = left;
-		else if (pheap->nodes[left].priority < pheap->nodes[left + 1].priority) pickedChild = left; 
-		//Minheap
-		else pickedChild = left + 1;
-		
-		if (last.priority > pheap->nodes[pickedChild].priority) { 
-			//Minheap
-			pheap->nodes[parentIndex] = pheap->nodes[pickedChild];
-			parentIndex = pickedChild;
-		}
-		else break;
-	}
-	pheap->nodes[parentIndex] = last;
-	pheap->size--;
-	return topitem;
+    const int size = pheap->size;
+    const Element topitem = pheap->nodes[1].item;
+    const HNode last = pheap->nodes[size];
+    int left, pickedChild, parentIndex = 1;
+    while ((left = parentIndex * 2) <= size) {
+        if (left == size) pickedChild = left;
+        else if (pheap->nodes[left].priority < pheap->nodes[left + 1].priority) pickedChild = left;
+        //Minheap
+        else pickedChild = left + 1;
+        if (last.priority > pheap->nodes[pickedChild].priority) {
+            //Minheap
+            pheap->nodes[parentIndex] = pheap->nodes[pickedChild];
+            parentIndex = pickedChild;
+        }
+        else break;
+    }
+    pheap->nodes[parentIndex] = last;
+    pheap->size--;
+    return topitem;
 }
 void AH_heapSort(Element* Base, int NumOfElement) {
-	ArrayHeap* heap = AH_new(NumOfElement);
-	for (int i = 0; i < NumOfElement; i++) 
-		AH_push(heap, Base[i], Base[i]);
-	for (int i = 0; i < NumOfElement; i++)
-		Base[i] = AH_pop(heap);
-	AH_delete(heap);
+    ArrayHeap* heap = AH_new(NumOfElement);
+    for (int i = 0; i < NumOfElement; i++)
+        AH_push(heap, Base[i], Base[i]);
+    for (int i = 0; i < NumOfElement; i++)
+        Base[i] = AH_pop(heap);
+    AH_delete(heap);
 }
 /*
 Integer Heap
@@ -116,7 +115,6 @@ int AH_pop(ArrayHeap* pheap) {
         else if (pheap->nodes[left] < pheap->nodes[left + 1]) pickedChild = left;
         //Minheap
         else pickedChild = left + 1;
-
         if (last > pheap->nodes[pickedChild]) {
             //Minheap
             pheap->nodes[parentIndex] = pheap->nodes[pickedChild];
@@ -130,14 +128,14 @@ int AH_pop(ArrayHeap* pheap) {
 }
 */
 
-/* 
+/*
 Below are only for isFull.
 
 int capacity;
 pheap->capacity = max + 1;
 
 int AH_isFull(ArrayHeap* pheap) {
-	return pheap->size == pheap->capacity - 1;
+    return pheap->size == pheap->capacity - 1;
 }
 */
 
